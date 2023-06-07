5bafb5a21a0948965a079a25dbfec656e3127c64
Wayne Dunkley <wdunkley84@gmail.com>
2023-06-07 12:24:17 +0100

    Formatting only

--- components/RoleFunction.vue
+++ components/RoleFunction.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="relative mr-32 mb-32 rounded-3xl overflow-hidden">
     <transition name="fade">
-      <div v-show="zoomedOut" class="absolute inset-0 z-10 flex items-center justify-center" :style="getBgColour('light')">
+      <div
+        v-show="zoomedOut"
+        class="absolute inset-0 z-10 flex items-center justify-center"
+        :style="getBgColour('light')"
+      >
         <div class="rounded-full text-family bg-white py-12 px-32">
           {{ roleFunction.roles.length }} roles
         </div>
@@ -15,82 +19,201 @@
       </div>
       <table class="border-collapse w-full">
         <tr class="text-2xl">
-          <th class="px-12 py-8 whitespace-no-wrap h-20 w-32">
-            Salary
-          </th>
-          <th v-for="familyRole in groupRolesByFamilyRole(roleFunction.roles)" :key="familyRole.name" class="px-12 py-8 whitespace-no-wrap h-20 text-left">
+          <th class="px-12 py-8 whitespace-no-wrap h-20 w-32">Salary</th>
+          <th
+            v-for="familyRole in groupRolesByFamilyRole(roleFunction.roles)"
+            :key="familyRole.name"
+            class="px-12 py-8 whitespace-no-wrap h-20 text-left"
+          >
             {{ familyRole.name }}
           </th>
         </tr>
         <tr>
-          <td class="px-12 py-8 whitespace-no-wrap h-56 flex items-center border-r-2 border-white" :style="getBgColour('dark')">
+          <td
+            class="px-12 py-8 whitespace-no-wrap h-56 flex items-center border-r-2 border-white"
+            :style="getBgColour('dark')"
+          >
             <div class="py-3 px-5 rounded-md font-semi-bold text-lg bg-white">
               $192k - $274k
             </div>
           </td>
-          <td v-for="familyRole in groupRolesByFamilyRole(roleFunction.roles)" :key="familyRole.name" class="border-r-2 border-white">
-            <transition-group name="list" tag="div" class="p-8 whitespace-no-wrap h-56 flex items-center" :style="getBgColour('dark')">
-              <role-panel v-for="role in rolesInGrade(familyRole.roles, 'PSSE Band 1')" :key="role.id" :role="role" class="mr-8" clickable zoomable @click.native="$emit('selected', role)" />
+          <td
+            v-for="familyRole in groupRolesByFamilyRole(roleFunction.roles)"
+            :key="familyRole.name"
+            class="border-r-2 border-white"
+          >
+            <transition-group
+              name="list"
+              tag="div"
+              class="p-8 whitespace-no-wrap h-56 flex items-center"
+              :style="getBgColour('dark')"
+            >
+              <role-panel
+                v-for="role in rolesInGrade(familyRole.roles, 'PSSE Band 1')"
+                :key="role.id"
+                :role="role"
+                class="mr-8"
+                clickable
+                zoomable
+                @click.native="$emit('selected', role)"
+              />
             </transition-group>
           </td>
         </tr>
         <tr>
-          <td class="px-12 py-8 whitespace-no-wrap h-56 flex items-center border-r-2 border-white">
+          <td
+            class="px-12 py-8 whitespace-no-wrap h-56 flex items-center border-r-2 border-white"
+          >
             <div class="py-3 px-5 rounded-md font-semi-bold text-lg bg-white">
               $142k - $164k
             </div>
           </td>
-          <td v-for="familyRole in groupRolesByFamilyRole(roleFunction.roles)" :key="familyRole.name" class="border-r-2 border-white">
-            <transition-group name="list" tag="div" class="p-8 whitespace-no-wrap h-56 flex items-center" :style="getBgColour('light')">
-              <role-panel v-for="role in rolesInGrade(familyRole.roles, '11-12')" :key="role.id" :role="role" class="mr-8" clickable zoomable @click.native="$emit('selected', role)" />
+          <td
+            v-for="familyRole in groupRolesByFamilyRole(roleFunction.roles)"
+            :key="familyRole.name"
+            class="border-r-2 border-white"
+          >
+            <transition-group
+              name="list"
+              tag="div"
+              class="p-8 whitespace-no-wrap h-56 flex items-center"
+              :style="getBgColour('light')"
+            >
+              <role-panel
+                v-for="role in rolesInGrade(familyRole.roles, '11-12')"
+                :key="role.id"
+                :role="role"
+                class="mr-8"
+                clickable
+                zoomable
+                @click.native="$emit('selected', role)"
+              />
             </transition-group>
           </td>
         </tr>
         <tr>
-          <td class="px-12 py-8 whitespace-no-wrap h-56 flex items-center border-r-2 border-white" :style="getBgColour('dark')">
+          <td
+            class="px-12 py-8 whitespace-no-wrap h-56 flex items-center border-r-2 border-white"
+            :style="getBgColour('dark')"
+          >
             <div class="py-3 px-5 rounded-md font-semi-bold text-lg bg-white">
               $122k - $135k
             </div>
           </td>
-          <td v-for="familyRole in groupRolesByFamilyRole(roleFunction.roles)" :key="familyRole.name" class="border-r-2 border-white">
-            <transition-group name="list" tag="div" class="p-8 whitespace-no-wrap h-56 flex items-center" :style="getBgColour('dark')">
-              <role-panel v-for="role in rolesInGrade(familyRole.roles, '9-10')" :key="role.id" :role="role" class="mr-8" clickable zoomable @click.native="$emit('selected', role)" />
+          <td
+            v-for="familyRole in groupRolesByFamilyRole(roleFunction.roles)"
+            :key="familyRole.name"
+            class="border-r-2 border-white"
+          >
+            <transition-group
+              name="list"
+              tag="div"
+              class="p-8 whitespace-no-wrap h-56 flex items-center"
+              :style="getBgColour('dark')"
+            >
+              <role-panel
+                v-for="role in rolesInGrade(familyRole.roles, '9-10')"
+                :key="role.id"
+                :role="role"
+                class="mr-8"
+                clickable
+                zoomable
+                @click.native="$emit('selected', role)"
+              />
             </transition-group>
           </td>
         </tr>
         <tr>
-          <td class="px-12 py-8 whitespace-no-wrap h-56 flex items-center border-r-2 border-white">
+          <td
+            class="px-12 py-8 whitespace-no-wrap h-56 flex items-center border-r-2 border-white"
+          >
             <div class="py-3 px-5 rounded-md font-semi-bold text-lg bg-white">
               $107k - $119k
             </div>
           </td>
-          <td v-for="familyRole in groupRolesByFamilyRole(roleFunction.roles)" :key="familyRole.name" class="border-r-2 border-white">
-            <transition-group name="list" tag="div" class="p-8 whitespace-no-wrap h-56 flex items-center" :style="getBgColour('light')">
-              <role-panel v-for="role in rolesInGrade(familyRole.roles, '7-8')" :key="role.id" :role="role" class="mr-8" clickable zoomable @click.native="$emit('selected', role)" />
+          <td
+            v-for="familyRole in groupRolesByFamilyRole(roleFunction.roles)"
+            :key="familyRole.name"
+            class="border-r-2 border-white"
+          >
+            <transition-group
+              name="list"
+              tag="div"
+              class="p-8 whitespace-no-wrap h-56 flex items-center"
+              :style="getBgColour('light')"
+            >
+              <role-panel
+                v-for="role in rolesInGrade(familyRole.roles, '7-8')"
+                :key="role.id"
+                :role="role"
+                class="mr-8"
+                clickable
+                zoomable
+                @click.native="$emit('selected', role)"
+              />
             </transition-group>
           </td>
         </tr>
         <tr>
-          <td class="px-12 py-8 whitespace-no-wrap h-56 flex items-center border-r-2 border-white" :style="getBgColour('dark')">
+          <td
+            class="px-12 py-8 whitespace-no-wrap h-56 flex items-center border-r-2 border-white"
+            :style="getBgColour('dark')"
+          >
             <div class="py-3 px-5 rounded-md font-semi-bold text-lg bg-white">
               $94k - $104k
             </div>
           </td>
-          <td v-for="familyRole in groupRolesByFamilyRole(roleFunction.roles)" :key="familyRole.name" class="border-r-2 border-white">
-            <transition-group name="list" tag="div" class="p-8 whitespace-no-wrap h-56 flex items-center" :style="getBgColour('dark')">
-              <role-panel v-for="role in rolesInGrade(familyRole.roles, '5-6')" :key="role.id" :role="role" class="mr-8" clickable zoomable @click.native="$emit('selected', role)" />
+          <td
+            v-for="familyRole in groupRolesByFamilyRole(roleFunction.roles)"
+            :key="familyRole.name"
+            class="border-r-2 border-white"
+          >
+            <transition-group
+              name="list"
+              tag="div"
+              class="p-8 whitespace-no-wrap h-56 flex items-center"
+              :style="getBgColour('dark')"
+            >
+              <role-panel
+                v-for="role in rolesInGrade(familyRole.roles, '5-6')"
+                :key="role.id"
+                :role="role"
+                class="mr-8"
+                clickable
+                zoomable
+                @click.native="$emit('selected', role)"
+              />
             </transition-group>
           </td>
         </tr>
         <tr>
-          <td class="px-12 py-8 whitespace-no-wrap h-56 flex items-center border-r-2 border-white">
+          <td
+            class="px-12 py-8 whitespace-no-wrap h-56 flex items-center border-r-2 border-white"
+          >
             <div class="py-3 px-5 rounded-md font-semi-bold text-lg bg-white">
               $80k - $87k
             </div>
           </td>
-          <td v-for="familyRole in groupRolesByFamilyRole(roleFunction.roles)" :key="familyRole.name" class="border-r-2 border-white">
-            <transition-group name="list" tag="div" class="p-8 whitespace-no-wrap h-56 flex items-center" :style="getBgColour('light')">
-              <role-panel v-for="role in rolesInGrade(familyRole.roles, '3-4')" :key="role.id" :role="role" class="mr-8" clickable zoomable @click.native="$emit('selected', role)" />
+          <td
+            v-for="familyRole in groupRolesByFamilyRole(roleFunction.roles)"
+            :key="familyRole.name"
+            class="border-r-2 border-white"
+          >
+            <transition-group
+              name="list"
+              tag="div"
+              class="p-8 whitespace-no-wrap h-56 flex items-center"
+              :style="getBgColour('light')"
+            >
+              <role-panel
+                v-for="role in rolesInGrade(familyRole.roles, '3-4')"
+                :key="role.id"
+                :role="role"
+                class="mr-8"
+                clickable
+                zoomable
+                @click.native="$emit('selected', role)"
+              />
             </transition-group>
           </td>
         </tr>
@@ -125,39 +248,35 @@ export default {
       required: true
     }
   },
-  data () {
+  data() {
     return {
       colours
     }
   },
   computed: {
-    zoomedOut () {
+    zoomedOut() {
       return this.zoom <= 0.15
     }
   },
   methods: {
-    groupRolesByFamilyRole (roles) {
+    groupRolesByFamilyRole(roles) {
       return this.$collect(this.roles)
         .where('familyFunction', this.roleFunction.name)
         .groupBy('familyRole')
         .keys()
-        .map(key => ({
+        .map((key) => ({
           name: key,
-          roles: this.$collect(roles)
-            .where('familyRole', key)
-            .all()
+          roles: this.$collect(roles).where('familyRole', key).all()
         }))
     },
-    getBgColour (type) {
+    getBgColour(type) {
       const colour = this.$collect(this.colours)
         .where('function', this.roleFunction.name)
         .first()
       return `background-color: ${colour[type]}`
     },
-    rolesInGrade (roles, level) {
-      return this.$collect(roles)
-        .where('grade', level)
-        .all()
+    rolesInGrade(roles, level) {
+      return this.$collect(roles).where('grade', level).all()
     }
   }
 }
