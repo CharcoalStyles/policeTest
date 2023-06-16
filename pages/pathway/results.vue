473bb91337640f2b2ddb903d1f803a73ecafe22b
Wayne Dunkley <wdunkley84@gmail.com>
2023-06-16 14:49:57 +0100

    Remove GA Tracking for questions that have been removed

--- pages/pathway/results.vue
+++ pages/pathway/results.vue
@@ -455,34 +455,6 @@ export default {
         eventAction: 'Government Employee',
         eventLabel: this.getHumanReadableAnswerValue('work-gov')
       })
-
-      // Question flow: Are you more interested in being an individual contributor or a manager?
-      this.$ga.event({
-        eventCategory: 'Pathway Answers',
-        eventAction: 'Manager or Contributor',
-        eventLabel: this.getHumanReadableAnswerValue('management')
-      })
-
-      // Question flow, Q21 - How many & which areas of interest are selected. "What discipline areas of digital are you interested in?"
-      this.$ga.event({
-        eventCategory: 'Pathway Answers',
-        eventAction: 'Discipline Areas Selected',
-        eventLabel: this.$store.state.pathway.answers.disciplines.value.length.toString()
-      })
-
-      // Question flow, Q23 - How many and which items items are selected for choosing a new role. "What matters to you when choosing a new role?"
-      this.$ga.event({
-        eventCategory: 'Pathway Answers',
-        eventAction: 'Role Priorities Selected',
-        eventLabel: this.$store.state.pathway.answers.priorities.value.length.toString()
-      })
-
-      // Question flow, Q23 - How many and which items items are selected for choosing a new role. "What matters to you when choosing a new role?"
-      this.$ga.event({
-        eventCategory: 'Pathway Answers',
-        eventAction: 'Learning Preferences Selected',
-        eventLabel: this.$store.state.pathway.answers.learning.value.length.toString()
-      })
     }
   }
 }
