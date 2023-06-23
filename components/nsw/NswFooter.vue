923c03b1336728afe50d5520c809c8da654f3e89
Wayne Dunkley <wdunkley84@gmail.com>
2023-06-23 11:51:37 +0100

    Add footer search

--- components/nsw/NswFooter.vue
+++ components/nsw/NswFooter.vue
@@ -15,7 +15,15 @@
         <div class="px-4 md:px-6 py-12 md:py-16">
           <div class="grid grid-cols-9 gap-12">
             <div class="col-span-9 lg:col-span-3">
-              <p class="text-lg font-bold mb-5">Search the PSC</p>
+              <label class="block text-lg font-bold mb-5" for="footer-search">Search the PSC</label>
+              <div>
+                <form class="relative" @submit="onSearch">
+                  <div class="flex items-center justify-center absolute top-0 left-0 bottom-0 text-nsw-grey-400 select-none pointer-events-none" style="width:44px;">
+                    <svg viewBox="0 0 16 16" width="16" height="16" fill="currentColor" class="css-0"><path d="M12.7 11.3c.9-1.2 1.4-2.6 1.4-4.2 0-3.9-3.1-7.1-7-7.1S0 3.2 0 7.1c0 3.9 3.2 7.1 7.1 7.1 1.6 0 3.1-.5 4.2-1.4l3 3c.2.2.5.3.7.3s.5-.1.7-.3c.4-.4.4-1 0-1.4l-3-3.1zm-5.6.8c-2.8 0-5.1-2.2-5.1-5S4.3 2 7.1 2s5.1 2.3 5.1 5.1-2.3 5-5.1 5z"></path></svg>
+                  </div>
+                  <input id="footer-search" v-model="search" class="nsw-form__input" type="text" value="" placeholder="Search" style="padding-left:36px;">
+                </form>
+              </div>
             </div>
             <div class="col-span-9 md:col-span-3 lg:col-span-2">
               <p class="text-lg font-bold mb-5">Connect with us</p>
@@ -94,6 +102,7 @@
 export default {
   data() {
     return {
+      search: '',
       link: {
         connect: [
           {
@@ -143,6 +152,14 @@ export default {
             href: 'https://www.psc.nsw.gov.au/legislation-and-policy'
           }
         ]
+      }
+    }
+  },
+  methods: {
+    onSearch(e) {
+      e.preventDefault()
+      if (this.search && this.search !== '') {
+        window.location.href = `https://www.psc.nsw.gov.au/search?q=${this.search}`
       }
     }
   }
