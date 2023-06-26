09b382f8c208769c4fea2d60b7eb72d754aa8a02
Wayne Dunkley <wdunkley84@gmail.com>
2023-06-26 15:12:09 +0100

    Hide salary fields if no role selected

--- components/pathway/results/RoleSelected.vue
+++ components/pathway/results/RoleSelected.vue
@@ -11,10 +11,10 @@
       {{ role.roleFunction }}
     </p>
     <div class="flex flex-col lg:flex-row">
-      <information-badge class="mb-6 rounded-full mr-3" size="sm">
+      <information-badge v-if="role.grade" class="mb-6 rounded-full mr-3" size="sm">
         Grade: {{ role.grade }}
       </information-badge>
-      <information-badge class="mb-6 rounded-full mr-3" size="sm" tooltip="Salaries are indicative only. Check the role description when applying.">
+      <information-badge v-if="role.salary.min && role.salary.max" class="mb-6 rounded-full mr-3" size="sm" tooltip="Salaries are indicative only. Check the role description when applying.">
         Salary: {{ $currency(role.salary.min) }} - {{ $currency(role.salary.max) }}
       </information-badge>
       <information-badge v-if="role.manager" size="sm" class="mb-6 rounded-full">
