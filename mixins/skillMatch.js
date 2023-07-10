449bdfc6480fc16991a82c06b16df26a4700acac
Wayne Dunkley <wdunkley84@gmail.com>
2023-07-10 08:36:41 +0200

    Update the skills match conditions

--- mixins/skillMatch.js
+++ mixins/skillMatch.js
@@ -4,26 +4,23 @@ export default {
     isNewSkill({ code, level }) {
       // Current role does not include code
       // Target role does include level (which is a number)
-      // Resource level is greater than assessed level
-      // Resource level is less than OR equal to target level
+      // Resource level is equal to the target role level
       return this.skillsAndCapabilitiesLevelMap?.current?.[code] === undefined &&
         typeof this.skillsAndCapabilitiesLevelMap?.target?.[code] === 'number' &&
-        parseInt(level) >= this.skillsAndCapabilitiesLevelMap?.assessed?.[code] &&
-        parseInt(level) <= this.skillsAndCapabilitiesLevelMap?.target?.[code]
+        parseInt(level) === this.skillsAndCapabilitiesLevelMap?.target?.[code]
     },
 
     // Pass in resource code and resource level
     isUpskill({ code, level }) {
       // Target role includes CODE
       // Current role includes CODE
-      // Resource LEVEL is greater than OR equal to target role LEVEL
-      // Resource LEVEL is less than OR equal to target LEVEL
-      // Resource LEVEL is greater than assessed LEVEL
+      // Resource level is equal to the target role level
+      // Resource level is greater than the self-assessed level
+      // The self-assessed level is less than the target level
       return this.skillsAndCapabilitiesLevelMap?.target?.[code] &&
       this.skillsAndCapabilitiesLevelMap?.current?.[code] &&
-        parseInt(level) >= this.skillsAndCapabilitiesLevelMap?.target?.[code] &&
+        parseInt(level) === this.skillsAndCapabilitiesLevelMap?.target?.[code] &&
         parseInt(level) > this.skillsAndCapabilitiesLevelMap?.assessed?.[code] &&
-        parseInt(level) <= this.skillsAndCapabilitiesLevelMap?.target?.[code] &&
         this.skillsAndCapabilitiesLevelMap?.assessed?.[code] < this.skillsAndCapabilitiesLevelMap?.target?.[code]
     }
   }
