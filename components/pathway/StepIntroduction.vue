770b1d7ab55e70f042dac140597340f09860e2b4
Aaron Styles <aaron@charcoalstyles.com>
2024-05-23 16:33:28 +1000

    First pass changes of question flow. Updated first page to match design (I don't think designs are done). Removed osme steps we're not using.

--- components/pathway/StepIntroduction.vue
+++ components/pathway/StepIntroduction.vue
@@ -3,83 +3,27 @@
     <div class="lg:w-7/12">
       <div class="mb-8 md:mb-10">
         <h2 class="btn-blue font-bold text-nsw-brand-primary-blue mb-3">
-          Procurement Career Pathways Tool
+          Career Pathways Tool
         </h2>
         <h1 class="text-3xl md:text-[42px] font-bold leading-tight mb-4">
           Get a personalised career recommendation and plan
         </h1>
-        <div class="flex items-center">
-          <img src="/clock-outline.svg" class="mr-2" />
-          <p class="text-nsw-grey-700">Estimated time: 8-10 mins</p>
-        </div>
       </div>
       <div class="mb-6">
         <h2 class="text-2xl font-bold mb-3">How it works</h2>
         <ul class="ml-5 list-outside list-disc leading-loose lg:w-5/6">
-          <li>Answer a few questions.</li>
-          <li>See roles you could move into.</li>
-          <li>Understand what skills you need to improve.</li>
-          <li>Get ways to upskill.</li>
+          <li>We’ll ask a few questions</li>
+          <li>We’ll show you what roles you could move into</li>
+          <li>We’ll help you understand what skills you need to improve</li>
+          <li>We’ll surface upskilling resources.</li>
         </ul>
       </div>
       <div class="mb-6">
         <h3 class="text-2xl font-bold mb-3">Who is this best suited for?</h3>
-        <p class="mb-3">People looking:</p>
         <ul class="ml-5 list-outside list-disc leading-loose lg:w-5/6">
-          <li>to develop their skills and capabilities</li>
+          <li>People looking for the next step in their career</li>
           <li>for the next step in their career</li>
-          <li>to work in a NSW public sector procurement role.</li>
-        </ul>
-      </div>
-      <div class="mb-6 lg:mb-0">
-        <h3 class="text-2xl font-bold mb-3">Notes about this tool</h3>
-        <ul class="ml-5 list-outside list-disc leading-loose">
-          <li>
-            Your responses are saved anonymously to your web browser. Clearing
-            your browser cache will reset your responses. This tool is regularly
-            updated. To see these updates you will need to clear your cache.
-          </li>
-          <li>
-            None of your information is stored on the Internet after you leave
-            this experience.
-          </li>
-          <li>
-            The Procurement Career Pathways tool is built using the
-            <a
-              href="https://www.psc.nsw.gov.au/workforce-management/capability-framework/the-capability-framework"
-              target="_blank"
-              class="text-nsw-brand-primary-blue underline font-bold"
-              style="text-underline-offset: 2px"
-              >NSW Public Sector Capability Framework</a
-            >
-            and
-            <a
-              href="https://www.psc.nsw.gov.au/workforce-management/capability-framework/occupation-specific-capability-sets/procurement-capability-set"
-              class="text-nsw-brand-primary-blue underline font-bold"
-              target="_blank"
-              style="text-underline-offset: 2px"
-              >Procurement Professionals Capability Set</a
-            >.
-          </li>
-          <li>
-            This tool compares the skills and capabilities of your
-            self-assessment against the focus capabilities required for your
-            current and target roles. Focus capabilities are those you need to
-            be recruited into a role. Complimentary capabilities are also
-            required, but these can be developed on the job. Check role
-            descriptions to see which capabilities are focus and complimentary.
-          </li>
-          <li>
-            For queries relating to the Procurement Career Pathway tool, please
-            contact the NSW Procurement Capability team at:
-            <a
-              href="mailto:NSWP_Capability@treasury.nsw.gov.au"
-              class="text-nsw-brand-primary-blue underline font-bold"
-              target="_blank"
-              style="text-underline-offset: 2px"
-              >NSWP_Capability@treasury.nsw.gov.au</a
-            >
-          </li>
+          <li>People looking to work in a NSW Government digital role</li>
         </ul>
       </div>
     </div>
