0d707c5536b48e34aaf5055db3202f509055aff8
Aaron Styles <aaron@charcoalstyles.com>
2024-06-05 16:36:44 +1000

    Made detective roles filter based on answer

--- pages/pathway/results.vue
+++ pages/pathway/results.vue
@@ -462,6 +462,9 @@ export default {
       return false
     },
     readableInterestsList() {
+      if (!this.answers.hasOwnProperty('interests')) {
+        return false
+      }
       return this.answers.interests.value.join(', ')
     },
     managementPreference() {
@@ -507,6 +510,10 @@ export default {
 
     familyRoles(currentRole) {
       const isPolicing = currentRole.jobFamily === 'Policing'
+      const showDetective = this.answers.hasOwnProperty('detective-roles')
+        ? this.answers['detective-roles'].value.includes('yes')
+        : false
+
       const familyFiltered = this.roles
         .filter((role) => role.jobFamily === currentRole.jobFamily)
         .filter((role) => {
@@ -530,6 +537,9 @@ export default {
 
       const sameGradeFamily = familyFiltered.filter((role) => {
         if (isPolicing) {
+          if (role.grade.split(' ')[0] === 'Detective' && !showDetective) {
+            return false
+          }
           return role.gradeId.grade === currentRole.gradeId.grade
         }
 
