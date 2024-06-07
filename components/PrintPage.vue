fd09ae28150212fdfafd25e3725ad48358af98f4
Aaron Styles <aaron@charcoalstyles.com>
2024-06-07 15:15:36 +1000

    Fixed a typo

--- components/PrintPage.vue
+++ components/PrintPage.vue
@@ -1,6 +1,6 @@
 <template>
   <button class="flex items-center underline print:hidden text-sm text-nsw-brand-primary-blue" style="text-underline-offset: 4px;">
-    <img src="/saveCopy.svg" alt="Savea copy icon" class="mr-3">
+    <img src="/saveCopy.svg" alt="Save a copy icon" class="mr-3">
     Save a copy
   </button>
 </template>
