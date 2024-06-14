5b97692c15b9f2faeadf4cc407f0b70fd60ffc02
Aaron Styles <aaron@charcoalstyles.com>
2024-06-14 17:18:27 +1000

    Added how it works

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,10 @@
         </p>
       </div>
     </hero-section>
-    <section-container title="Explore roles and make a personalised career plan" :cols="2">
+    <section-container
+      title="Explore roles and make a personalised career plan"
+      :cols="2"
+    >
       <feature-panel
         image="/images/rescueDrone.png"
         alt="Role Explorer"
@@ -50,62 +53,47 @@
       </feature-panel>
     </section-container>
 
-    <div class="bg-nsw-brand-purple-dark py-16 md:py-20 my-12 md:my-16">
-      <div
-        class="flex flex-col lg:flex-row px-4 md:px-6 xl:container space-y-6 lg:space-y-0 lg:space-x-6"
-      >
-        <div class="flex flex-col flex-1 justify-center">
-          <div class="text-white">
-            <!-- <div class="font-bold mb-4">
-              Role explorer tool
-            </div> -->
-            <h2
-              class="font-bold text-3xl xl:text-[32px] leading-tight xl:w-11/12 mb-4"
-            >
-              Learn more about working in procurement
-            </h2>
-            <p class="w-11/12 mb-4">
-              Hear about what it’s like to work in public sector procurement,
-              including the benefits and opportunities.
+    <section-container title="How it works">
+      <div class="flex flex-col items-center md:items-stretch md:flex-row flex-wrap container">
+        <div class="flex flex-col w-full md:w-1/2 lg:w-1/4">
+          <img src="/figures/1.svg" class="h-64 mb-6" />
+          <div class="mx-6">
+            <p class="font-bold mb-2">Explore Roles</p>
+            <p>Explore roles and pathways within NSWPF.</p>
+          </div>
+        </div>
+
+        <div class="flex flex-col w-full md:w-1/2 lg:w-1/4">
+          <img src="/figures/2.svg" class="h-64 mb-6" />
+          <div class="mx-6">
+            <p class="font-bold mb-2">Get personalised career pathways</p>
+            <p>
+              Explore and prioritise career pathways based on what matters to
+              you.
             </p>
           </div>
-          <!-- <div>
-            <nuxt-link to="/explorer" class="inline-flex items-center font-bold rounded-md h-10 md:h-12 px-6 md:px-8 text-sm md:text-base bg-white text-nsw-brand-grey-primary">
-              Explore roles
-            </nuxt-link>
-          </div> -->
         </div>
-        <div class="flex-1">
-          <client-only>
-            <vimeo-player
-              ref="player"
-              class="vimeo-video"
-              :video-id="videoID"
-              :player-height="height"
-              :controls="true"
-              video-url="https://vimeo.com/357726193/b1773b87c6"
-              @ready="onReady"
-            ></vimeo-player>
-          </client-only>
-          <!-- <img src="/images/home-hero.jpg" alt="" class="w-full object-cover" style="aspect-ratio: 16/9;" /> -->
-        </div>
-      </div>
-    </div>
 
-    <section-container title="How this tool can help you" :cols="1">
-      <div class="lg:w-10/12 lg:my-8">
-        <img
-          src="images/feature-comparison.png"
-          alt="Skills and capabilities comparison table"
-          class="w-full object-cover mb-8"
-        />
-        <h3 class="font-bold text-lg leading-tight xl:w-11/12 mb-4">
-          Understand the skills required for future roles
-        </h3>
-        <p class="w-11/12 mb-4">
-          Compare current and desired roles side by side to see the skills you
-          may need to build or improve on.
-        </p>
+        <div class="flex flex-col w-full md:w-1/2 lg:w-1/4">
+          <img src="/figures/3.svg" class="h-64 mb-6" />
+          <div class="mx-6">
+            <p class="font-bold mb-2">
+              Understand the skills required for future roles
+            </p>
+            <p>
+              Compare current and desired roles side by side to see the skills
+              you may need to build or improve on.
+            </p>
+          </div>
+        </div>
+
+        <div class="flex flex-col w-full md:w-1/2 lg:w-1/4">
+          <img src="/figures/4.svg" class="h-64 mb-6" />
+          <div class="mx-6">
+            <p class="font-bold mb-2">Save your plan</p>
+            <p>Save your plan and use it to start a career conversation.</p>
+          </div>
+        </div>
       </div>
     </section-container>
   </div>
